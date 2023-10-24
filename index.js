8fdfb55ddc3e0272050f45d31ea1d18eca938f3f
Agustin Migliorero <agustinmigliorero@hotmail.com>
2023-10-23 21:43:03 -0300

    Commit fin de la clase

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/express-programacion", {
